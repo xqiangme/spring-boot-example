99d2a869020089d1647469ad18f7f78e2d227a27
mengqiang <mengqiangme@163.com>
2020-04-28 00:17:20 +0800

    no message

--- springBoot-redis(jedis)/src/main/java/com/example/redis/cache/aspect/CacheAspect.java
+++ springBoot-redis(jedis)/src/main/java/com/example/redis/cache/aspect/CacheAspect.java
@@ -96,7 +96,8 @@ public class CacheAspect {
         lock.lock();
         try {
             //二次尝试从缓存中读取
-            byte[] cacheByteValueSecond = this.getFromRedis(cacheKey);
+            //byte[] cacheByteValueSecond = this.getFromRedis(cacheKey);
+            byte[] cacheByteValueSecond = jedis.get(cacheKey);
             if (null != cacheByteValueSecond && cacheByteValueSecond.length > 0) {
                 //反序列化对象
                 value = SerializableUtil.deserialize(cacheByteValueSecond);
@@ -106,7 +107,7 @@ public class CacheAspect {
                 log.info("[ CacheAspect ] >> cacheKey:" + new String(cacheKey) + ",class:" + className + ",method:" + methodName + " second get data from cache ");
                 return value;
             }
-            //如果设置了存储null值，并且key存在 > 则直接返回 null
+            //如果设置了允许存储null值，并且key存在 > 则直接返回 null
             if (cacheData.storageNullFlag() && jedis.exists(cacheKey)) {
                 log.info("[ CacheAspect ] >> cacheKey:" + new String(cacheKey) + ",class:" + className + ",method:" + methodName + " second get data from cache  is null ");
                 return null;
@@ -117,14 +118,17 @@ public class CacheAspect {
 
             //返回值不为空-存入缓存并返回
             if (null != value) {
-                setToRedis(cacheKey, value, getExpireTime(cacheData));
+                //setToRedis(cacheKey, value, getExpireTime(cacheData));
+                jedis.setex(cacheKey, getExpireTime(cacheData), SerializableUtil.serialize(value));
+
                 return value;
             }
 
             //返回值不为空-是否需要存储空对象
             if (cacheData.storageNullFlag()) {
                 //存入缓存,value-也存储key
-                setToRedis(cacheKey, cacheKey, getExpireTime(cacheData));
+                //setToRedis(cacheKey, cacheKey, getExpireTime(cacheData));
+                jedis.setex(cacheKey, getExpireTime(cacheData), SerializableUtil.serialize(value));
                 return null;
             }
         } finally {
@@ -133,11 +137,21 @@ public class CacheAspect {
             //关闭
             if (jedis.isConnected()) {
                 jedis.close();
+                log.info("[ CacheAspect ] >> cacheKey:" + new String(cacheKey) + ",class:" + className + ",method:" + methodName + " close jedis client ");
             }
         }
         return null;
     }
 
+    /**
+     * 生成缓存key
+     *
+     * @param cacheData
+     * @param className
+     * @param methodName
+     * @param pjpParam
+     * @return
+     */
     private String getCacheKey(CacheData cacheData, String className, String methodName, ProceedingJoinPoint pjpParam) {
         //缓存key前缀
         String keyPrefix = cacheData.keyPrefix();
@@ -153,11 +167,12 @@ public class CacheAspect {
         Annotation[][] annotations = ((MethodSignature) pjpParam.getSignature()).getMethod().getParameterAnnotations();
         List<Integer> ignoreList = new ArrayList<>();
         for (int i = 0; i < annotations.length; i++) {
-            if (annotations[i].length > 0) {
-                for (int j = 0; j < annotations[i].length; j++) {
-                    if (annotations[i][j].annotationType().equals(IgnoreCache.class)) {
-                        ignoreList.add(i);
-                    }
+            if (annotations[i].length <= 0) {
+                continue;
+            }
+            for (int j = 0; j < annotations[i].length; j++) {
+                if (annotations[i][j].annotationType().equals(IgnoreCache.class)) {
+                    ignoreList.add(i);
                 }
             }
         }
@@ -176,69 +191,6 @@ public class CacheAspect {
         return keyPrefix + POINT + DigestUtils.md5Hex(methodPath + paramKey);
     }
 
-    protected void setToRedis(byte[] key, Object value, int expire) {
-        if (expire == 0) {
-            expire = (int) (60 * 60 * 24 - ((System.currentTimeMillis() / 1000 + 8 * 3600) % (60 * 60 * 24)));
-        }
-
-        Jedis jedis = null;
-        try {
-            jedis = jedisPool.getResource();
-            String setex = jedis.setex(key, expire, SerializableUtil.serialize(value));
-            System.out.println(setex);
-        } catch (Exception e) {
-
-        } finally {
-            try {
-                if (jedis != null) {
-                    if (jedis.isConnected()) {
-                        jedis.close();
-                    }
-                }
-            } catch (Exception e) {
-            }
-        }
-    }
-
-    protected byte[] getFromRedis(byte[] key) {
-        if (key == null) {
-            return null;
-        }
-        Jedis jedis = null;
-        try {
-            jedis = jedisPool.getResource();
-            return jedis.get(key);
-        } catch (Exception e) {
-            // LOGGER.error("[Redis-invoke] Exception {} - {}", ex.getMessage(), ex);
-        } finally {
-            if (jedis != null) {
-                if (jedis.isConnected()) {
-                    jedis.close();
-                }
-            }
-        }
-        return null;
-    }
-
-    protected Boolean exists(byte[] key) {
-        if (key == null) {
-            return false;
-        }
-        Jedis jedis = null;
-        try {
-            jedis = jedisPool.getResource();
-            return jedis.exists(key);
-        } catch (Exception e) {
-
-        } finally {
-            if (jedis != null) {
-                if (jedis.isConnected()) {
-                    jedis.close();
-                }
-            }
-        }
-        return false;
-    }
 
     /**
      * 计算过期时间 如果缓存设置了需要延迟失效，取设置的延迟时间1-2倍之间的一个随机值作为真正的延迟时间值
@@ -254,4 +206,70 @@ public class CacheAspect {
         int offset = 0;
         return expire + offset;
     }
+    
+
+//    protected void setToRedis(byte[] key, Object value, int expire) {
+//        if (expire == 0) {
+//            expire = (int) (60 * 60 * 24 - ((System.currentTimeMillis() / 1000 + 8 * 3600) % (60 * 60 * 24)));
+//        }
+//
+//        Jedis jedis = null;
+//        try {
+//            jedis = jedisPool.getResource();
+//            String setex = jedis.setex(key, expire, SerializableUtil.serialize(value));
+//            System.out.println(setex);
+//        } catch (Exception e) {
+//
+//        } finally {
+//            try {
+//                if (jedis != null) {
+//                    if (jedis.isConnected()) {
+//                        jedis.close();
+//                    }
+//                }
+//            } catch (Exception e) {
+//            }
+//        }
+//    }
+
+//    protected byte[] getFromRedis(byte[] key) {
+//        if (key == null) {
+//            return null;
+//        }
+//        Jedis jedis = null;
+//        try {
+//            jedis = jedisPool.getResource();
+//            return jedis.get(key);
+//        } catch (Exception e) {
+//            // LOGGER.error("[Redis-invoke] Exception {} - {}", ex.getMessage(), ex);
+//        } finally {
+//            if (jedis != null) {
+//                if (jedis.isConnected()) {
+//                    jedis.close();
+//                }
+//            }
+//        }
+//        return null;
+//    }
+
+//    protected Boolean exists(byte[] key) {
+//        if (key == null) {
+//            return false;
+//        }
+//        Jedis jedis = null;
+//        try {
+//            jedis = jedisPool.getResource();
+//            return jedis.exists(key);
+//        } catch (Exception e) {
+//
+//        } finally {
+//            if (jedis != null) {
+//                if (jedis.isConnected()) {
+//                    jedis.close();
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
 }

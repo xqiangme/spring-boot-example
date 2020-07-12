c545b70aca4cea63f1d451920d5fccf3b4f02f5e
mengqiang <mengq@haifenbb.com>
2020-07-12 17:46:08 +0800

    SSO单点

---
+++ spring-boot-sso/README.md
@@ -0,0 +1,8 @@
+sso统一认证单点登录demo项目
+
+
+sso-server SS0统一认证中心服务
+
+sso-client1 客户端1
+
+sso-client2 客户端2
--- spring-boot-sso/sso-client2/src/main/java/com/example/sso/client2/controller/IndexController.java
+++ spring-boot-sso/sso-client2/src/main/java/com/example/sso/client2/controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/index")
+    @RequestMapping({"/", "/index"})
     protected String index(Model model) {
         model.addAttribute("logOutUrl", SSOClientHelper.getSSOLogOutUrl());
         return "index";
